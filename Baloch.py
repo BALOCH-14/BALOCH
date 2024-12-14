9eb7182737c5e1a76e3dc35d36fe6b32e57f9e40
BALOCH <138492420+BALOCH-14@users.noreply.github.com>
2024-12-14 10:05:36 +0500

    Update Baloch.py

--- Baloch.py
+++ Baloch.py
@@ -3,7 +3,7 @@ os.system('pkg install espeak -y --quiet 2>/dev/null')
 os.system("clear")
 os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
-print('\033[92;1m [\033[97;1mâ€¢\033[92;1m] Join Whatsapp Group')
+print('\033[92;1m [\033[97;1•\033[92;1m] Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/GWHBCHMKcZG7jnj9Tcy1A2')
 Baloch=platform.architecture()[0]
 if Baloch=="32bit":
